ac457c0f199a4cad7c155029a407cd6daae8b57f
terra.caines <terra.caines@efaed70c-7552-0410-a884-af111aeb2168>
2008-07-28 21:51:34 +0000

    roundclick

---
+++  zclack/zclack/src/main/RoundMovements.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import clicks.iclick;
+
+public class RoundMovements extends Buttons {
+	
+	
+	
+	void initialize() {
+		this.setLayout(new FlowLayout());
+		this.setSize(100, 30);
+		this.setTitle("ZClack");
+
+		add(getCurrent());
+		//add(getTime());
+		
+		add(getGuess());
+		
+		java.awt.Dimension screenSize = Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		setLocation(screenSize.width - 100, screenSize.height - 30);
+		setAlwaysOnTop(true);
+
+	}
+
+	boolean shouldProcess = false;
+
+	MouseGesturesRecognizer mgl;
+
+	@Override
+	public void runButtons(int count) {
+		// TODO Auto-generated method stub
+		if (count > click.interval/2) {
+			mgl.clearTemporaryInfo();
+			shouldProcess = true;
+		}
+		if (shouldProcess)
+			mgl.processMouseEvent();
+	}
+
+	@Override
+	public void setupButtons(Map<String, iclick> buttonMap) {
+		// TODO Auto-generated method stub
+		this.buttonMap = buttonMap;
+	}
+
+	Map<String, iclick> buttonMap;
+
+}
